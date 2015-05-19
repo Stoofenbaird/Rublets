31cc37f01500bedfc5ddbdc07cf86a4c967ac774
Stoofenbaird <mim@stoofenbaird.com>
2015-05-18 22:14:02 -0500

    Changed program structure so that it now runs.

--- Storytime.rb
+++ Storytime.rb
@@ -1,49 +1,30 @@
-def intro
+#!/usr/bin/env ruby
+# May 18, 2015 by Stoofenbaird. A Rublet to tell a story.  Uses case statement flow control, 
+# string and number manipulation.
 	print "Hello. I'll tell you a story, but I need some information from you. Tell me your name: "
 	name = gets.chomp
-	return name.downcase.reverse.capitalize
-end
-
-def color
 	print "What is your favorite color? "
-	return color = gets.chomp.downcase
-end
-
-def plant
+	color = gets.chomp.downcase
 	print "Name a plant that you like: "
-	plant = gets.chomp
-	return plant.downcase
-end
-
-def city
+	plant = gets.chomp.downcase
 	print "Name a city you'd like to visit: "
-	return city = gets.chomp
-end
-
-def nums
+	city = gets.chomp.capitalize
 	print "Choose a number between 1 and 32: "
 	nums = gets.chomp.to_f
-	return nums
-end
-
-def story
-	intro
-	plant
-	city
-	nums
-	case color
+	
+case color
 	when "red"
 		name = (name + " Red" + plant.reverse)
 		nums1 = nums * 11
 		nums2 = nums/6
-		puts "A turtle named " + name + "traveled " + nums2.to_s + " hours to get to a watering hole.
-		While there, he encountered a fox with " + nums1.to_s + " grapes. The fox let " + name + "  eat
+		puts "A turtle named " + name + " traveled " + nums2.to_s + " hours to get to a watering hole.
+		While there, he encountered a fox with " + nums1.to_s + " grapes. The fox let " + name + " eat
 		half the grapes.  When " + name + " went into his house for a nap, the fox picked him up,
 		carried him to " + city + " where he found a very good restaurant willing to put the turtle
 		in a pot and cook him up."
 
 	when "blue"
-		name = "La " + name + "reoux"
+		name = ("La " + name + "reoux")
 		nums3 = nums + 1 * 3
 		nums4 = nums * 1000
 		puts "The great French aviator " + name + " flew his derigible across three oceans 
@@ -60,9 +41,10 @@ def story
 		feeling got, the more " + color + "ish she turned until she completely blended in and no one ever
 		saw her again."
 	end
-end
 
 
 
-story()
+	
+	
+
 
